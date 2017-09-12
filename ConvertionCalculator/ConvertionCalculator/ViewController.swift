ae8e84b81bb483e33d1fd5a0e43893bad592e647
Shi Yifan <shiyifan@mwc-055110.dhcp.missouri.edu>
2017-09-12 13:42:51 -0500

    Update

--- ConvertionCalculator/ConvertionCalculator/ViewController.swift
+++ ConvertionCalculator/ConvertionCalculator/ViewController.swift
@@ -9,42 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    @IBOutlet weak var picker: UIPickerView!
-    
-    var pickerData: [[String]] = [[String]]()
-    
-    override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            
-            self.picker.delegate = self
-            self.picker.dataSource = self
-
-            // Input data into the Array:
-            pickerData = [["inch","yard"],["in","yrd"]]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
+    
+    
+   
+    
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 2
+//    }
+//
+  @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.picker.delegate = self
+        self.picker.dataSource = self
+
+        // Input data into the Array:
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
     
+    // The number of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerData.count
     }
     
+    // The data to return for the row and component (column) that's being passed in
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[component][row]
+        return pickerData[row]
     }
-
-
-
+    
+    
 }
-
-
